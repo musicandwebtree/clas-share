2fcc6a59bd3cf980d0a601b5029e3ed9baf1c24f
4QUALIA\tomoko.yasui <tomoko.yasui@4qualia.co.jp>
2025-05-02 18:44:39 +0900

    feat: bootstrap Next.js app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-// src/app/page.tsx
-export default function Home() {
-  return (
     <main className="flex h-screen items-center justify-center text-3xl">
       Hello ClasShare!
     </main>
-  );
-}
-
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
